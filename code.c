dd20f1a68dab0a86eb4dddd7f48a248e9158dad0
sk1zls <130976550+sk1zls@users.noreply.github.com>
2023-10-07 18:39:17 +0300

    Update code.c

--- code.c
+++ code.c
@@ -1,69 +1,131 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Encoder.h>
 
-const int buttonUpPin = 2; // пин для кнопки меню вверх
-const int buttonDownPin = 3; // пин для кнопки меню вниз
-const int buttonSelectPin = 4; // пин для кнопки выбора пункта
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+Encoder encoder(A3, A2);
 
-// Инициализируем дисплей
-LiquidCrystal_I2C lcd(32, 16, 2);
+const int joySelPin = A1;
+const int speakerPin = 8;
+const int buttonPin = 9; // Пин для кнопки включения/выключения звука
 
-int menuState = 0; // текущее состояние меню
-int timerValue = 60; // значение времени обратного отсчета по умолчанию
+bool soundEnabled = true; // Переменная для отслеживания состояния звука (включен или выключен)
+
+unsigned long timerStart = 0;
+unsigned long elapsedTime = 0;
+unsigned long setTime = 0;
+bool counting = false;
+bool paused = false;
+unsigned long pausedTime = 0;
+unsigned long pausedSetTime = 0;
 
 void setup() {
-  pinMode(buttonUpPin, INPUT_PULLUP); // настраиваем пин кнопки меню вверх на вход с подтяжкой
-  pinMode(buttonDownPin, INPUT_PULLUP); // настраиваем пин кнопки меню вниз на вход с подтяжкой
-  pinMode(buttonSelectPin, INPUT_PULLUP); // настраиваем пин кнопки выбора пункта на вход с подтяжкой
-  Wire.begin(); // инициализируем I2C шину
-  lcd.init(); // инициализируем дисплей
-  lcd.backlight(); // включаем подсветку дисплея
+  pinMode(joySelPin, INPUT_PULLUP);
+  pinMode(speakerPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // Устанавливаем кнопку с подтяжкой к питанию
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Set time (mins): ");
+  lcd.setCursor(15, 0); // Позиция для вывода состояния звука
+  lcd.print(soundEnabled ? "S" : "M"); // Вывод "S" или "M" в зависимости от состояния звука
 }
 
 void loop() {
-  int buttonUpState = digitalRead(buttonUpPin); // считываем состояние кнопки меню вверх
-  int buttonDownState = digitalRead(buttonDownPin); // считываем состояние кнопки меню вниз
-  int buttonSelectState = digitalRead(buttonSelectPin); // считываем состояние кнопки выбора пункта
+  long newPosition = encoder.read() / 4;
 
-  // обрабатываем нажатие кнопки меню вверх
-  if (buttonUpState == LOW) {
-    if (menuState == 0) {
-      menuState = 1; // переключаемся на пункт меню "Установить время"
-    } else if (menuState == 1) {
-      timerValue++; // увеличиваем значение времени обратного отсчета при выборе пункта "Добавить"
+  if (newPosition != setTime) {
+    setTime = newPosition;
+    elapsedTime = setTime * 60;
+
+    lcd.setCursor(0, 1);
+    lcd.print("                 ");
+    lcd.setCursor(0, 1);
+    lcd.print(setTime);
+
+    counting = false; // Обнуляем таймер при изменении установленного времени
+  }
+
+  int joySelState = digitalRead(joySelPin);
+
+  if (joySelState == LOW) {
+    delay(500);
+
+    if (!counting) {
+      if (!paused) {
+        timerStart = millis();
+      } else {
+        // Если была пауза, продолжаем с сохраненным установленным временем
+        timerStart = millis() - pausedTime;
+        paused = false;
+        setTime = pausedSetTime; // Устанавливаем время из сохраненного значения
+      }
+
+      counting = true;
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Counting Down: ");
+      lcd.setCursor(15, 0); // Позиция для вывода состояния звука
+      lcd.print(soundEnabled ? "S" : "M"); // Вывод "S" или "M" в зависимости от состояния звука
+    } else {
+      counting = false; // Останавливаем таймер при повторном нажатии
+      paused = true;
+      pausedTime = millis() - timerStart; // Запоминаем прошедшее время при паузе
+      pausedSetTime = setTime; // Сохраняем установленное время перед паузой
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Paused: ");
+      lcd.setCursor(15, 0); // Позиция для вывода состояния звука
+      lcd.print(soundEnabled ? "S" : "M"); // Вывод "S" или "M" в зависимости от состояния звука
     }
-    delay(200); // задержка для подавления дребезга контактов кнопки
   }
 
-  // обрабатываем нажатие кнопки меню вниз
-  if (buttonDownState == LOW) {
-    if (menuState == 0) {
-      menuState = 1; // переключаемся на пункт меню "Установить время"
-    } else if (menuState == 1) {
-      timerValue--; // уменьшаем значение времени обратного отсчета при выборе пункта "Убавить"
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW) {
+    // При нажатии кнопки меняем состояние звука и выводим соответствующее сообщение
+    soundEnabled = !soundEnabled;
+    lcd.setCursor(15, 0); // Позиция для вывода состояния звука
+    lcd.print(soundEnabled ? "S" : "M"); // Вывод "S" или "M" в зависимости от состояния звука
+    delay(500);  // Задержка для предотвращения дребезга кнопки
+  }
+
+  if (counting) {
+    unsigned long currentTime = millis();
+    unsigned long elapsedTimeMillis = currentTime - timerStart;
+    elapsedTime = setTime * 60 - (elapsedTimeMillis / 1000);
+
+    if (elapsedTime <= 0) {
+      counting = false;
+
+      for (int i = 0; i < 10; i++) {
+        if (soundEnabled) {
+          tone(speakerPin, 440, 100);
+          delay(100);
+          noTone(speakerPin);
+          delay(100);
+        }
+      }
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Time's up!");
+      lcd.setCursor(0, 1);
+      lcd.print("Press restart button");
+      return;
     }
-    delay(200); // задержка для подавления дребезга контактов кнопки
-  }
 
-  // обрабатываем нажатие кнопки выбора пункта
-  if (buttonSelectState == LOW) {
-    if (menuState == 0) {
-      // здесь можно добавить код для выполнения обратного отсчета
-    } else if (menuState == 1) {
-      menuState = 0; // возвращаемся в главное меню
+    lcd.setCursor(0, 1);
+    lcd.print(elapsedTime / 60);
+    lcd.print(":");
+    if ((elapsedTime % 60) < 10) {
+      lcd.print("0");
     }
-    delay(200); // задержка для подавления дребезга контактов кнопки
-  }
+    lcd.print(elapsedTime % 60);
 
-  // выводим текущее состояние меню и значение времени обратного отсчета на дисплей
-  lcd.setCursor(0, 0);
-  lcd.print("Menu state: ");
-  if (menuState == 0) {
-    lcd.print("Main");
-  } else if (menuState == 1) {
-    lcd.print("Set time");
+    delay(200);
   }
-  lcd.setCursor(0, 1);
-  lcd.print("Timer value: ");
-  lcd.print(timerValue);
 }
